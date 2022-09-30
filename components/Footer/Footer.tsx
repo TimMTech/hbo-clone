ef3945cc4eb6f3a412955317e648813bb3290090
TimMTech <tmukhamedov97@gmail.com>
2022-09-30 17:58:16 -0400

    Added backdrop image to footer CTA

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,39 +1,51 @@
-
 import {
   AiFillGithub,
   AiFillInstagram,
   AiFillFacebook,
   AiFillLinkedin,
 } from "react-icons/ai";
-
+import NextImage from "next/image";
+import NextLink from "next/link";
+import test_image from "/public/static/image/test_image.png";
 
 const Footer: React.FC = () => {
-  
   return (
-    <footer className="md:flex md:flex-row-reverse md:items-center md:justify-between w-screen text-sm h-[200px] flex flex-col justify-end px-8 pb-4 gap-4">
-      <p className="md:text-lg">&copy; 2022 TimMTech, LLC. All Rights Reserved.</p>
-      <ul className="flex items-center gap-2">
-        <li>
-          <a>
-            <AiFillGithub size={30} />
-          </a>
-        </li>
-        <li>
-          <a>
-            <AiFillFacebook size={30} />
-          </a>
-        </li>
-        <li>
-          <a>
-            <AiFillInstagram size={30} />
-          </a>
-        </li>
-        <li>
-          <a>
-            <AiFillLinkedin size={30} />
-          </a>
-        </li>
-      </ul>
+    <footer className="w-screen flex flex-col relative">
+      <div className="h-[500px] w-full relative  ">
+        <NextImage src={test_image} layout="fill" objectFit="cover" />
+      </div>
+      <div className="absolute top-[40%] left-[7%] text-white">
+        <h1 className="text-2xl">Already Have an HBO Max Account?</h1>
+        <button className="hover:bg-none hover:bg-black hover:shadow-[inset_0_0_0_2px] hover:shadow-indigo-600 px-6 py-2 bg-gradient-to-r from-indigo-600 to-purple-600 rounded-md ">
+          <NextLink href="/auth/subscribe">SIGN UP</NextLink>
+        </button>
+      </div>
+      <div className="p-8">
+        <p>&copy; 2022 TimMTech, LLC. All Rights Reserved.</p>
+
+        <ul className="flex items-center gap-2">
+          <li>
+            <a>
+              <AiFillGithub size={30} />
+            </a>
+          </li>
+          <li>
+            <a>
+              <AiFillFacebook size={30} />
+            </a>
+          </li>
+          <li>
+            <a>
+              <AiFillInstagram size={30} />
+            </a>
+          </li>
+          <li>
+            <a>
+              <AiFillLinkedin size={30} />
+            </a>
+          </li>
+        </ul>
+      </div>
     </footer>
   );
 };
